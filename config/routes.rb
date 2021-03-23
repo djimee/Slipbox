e379bde29779865cbc1269d66ebdd7ff3a6e1691
James Horn <jhorn2@sheffield.ac.uk>
2021-03-23 12:25:26 +0000

    Created edit notes page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'edit/index'
   match "/403", to: "errors#error_403", via: :all
 
   get :"myslipboxes", to: "myslipboxes#index"
   get :"signin", to: "signin#index"
   get :"signup", to: "signup#index"
   get :"threads", to: "threads#index"
+  get :"edit", to: "edit#index"
   get :ie_warning, to: 'errors#ie_warning'
   get :javascript_warning, to: 'errors#javascript_warning'
   
