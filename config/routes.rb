37464aca8458bb4a161f819818079bc6b41d2ea7
James Horn <jhorn2@sheffield.ac.uk>
2021-03-31 17:07:45 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  get 'test/idnex'
+  get 'edit/index'
   match "/403", to: "errors#error_403", via: :all
-  match "/404", to: "errors#error_404", via: :all
-  match "/422", to: "errors#error_422", via: :all
-  match "/500", to: "errors#error_500", via: :all
 
+  get :"myslipboxes", to: "myslipboxes#index"
+  get :"signin", to: "signin#index"
+  get :"signup", to: "signup#index"
+  get :"threads", to: "threads#index"
+  get :"edit", to: "edit#index"
+  get :"mynotes", to: "mynotes#index"
+  get :"test", to: "test#index"
   get :ie_warning, to: 'errors#ie_warning'
-  get :javascript_warning, to: 'errors#javascript_warning'
 
-  root to: "pages#home"
+  get :javascript_warning, to: 'errors#javascript_warning'
+  
+
+  # root to: "pages#home"
+  root to: "myslipboxes#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
