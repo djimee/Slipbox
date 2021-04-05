3034485afe7b31907e67faaf6fc57bd2093cefbe
James Horn <jhorn2@sheffield.ac.uk>
2021-04-05 21:48:56 +0100

    Fixed adding nodes bug

--- app/assets/javascripts/threads.js
+++ app/assets/javascripts/threads.js
@@ -12,6 +12,7 @@ class Node {
     getId() { return this.id; }
     getName() { return this.name; }
     getChildren() { return this.children; }
+
     getRecursion() { return this.recursion; }
 
     setRecursion(rec) { this.recursion = rec; }
@@ -28,45 +29,31 @@ class GenerateTree {
 
     findRootNode(nodeList) {
         let items = nodeList[0].split(":");
-        return new Node(items[0], items[1]);
+        return new Node(items[0], items[1], null);
     }; 
 
-    addNode(tree, id, node) {
+    searchChildNodes(childNodes, node) {
+        childNodes.foreach((n) => {if (n.getId() == node.getId()) {return n}});
+        return null;
+    }
 
-        //children node cap
-        if (tree.getChildren().length > this.NODE_REC_LIMIT) {
-            tree.resetChildren();
-            tree.setRecursion(false);
-        }
-        if (tree.getRecursion() == true) {
-            if (tree.getChildren().length > 0) {
-                //recurse on child node
-                for (let c in tree.getChildren()) {
-                    
-                    let childID = tree.getChildren()[c].getId();
-                    if (childID.substring(0, id+1) == node.substring(0, id+1)) {
-                        if (childID.substring(0, id+2) == node.substring(0, id+2)) {
-                            this.addNode(tree.getChildren()[c], id + 1, node);                        
-                        } else {
-                            let items = node.split(":");     
-                            tree.getChildren().push(new Node(items[0], items[1]));
-                            
-                        }
-                        break;
-                    }
-                }   
-                                
-            } else {
-                
-                let items = node.split(":");
-                tree.getChildren().push(new Node(items[0], items[1]));
-            }
+    addNode(root, parent, node) {
+
+        let parentId = parent.split(":")[0];
+        if (root.getId() == parentId) {
+            let info = node.split(":");
+            root.getChildren().push(new Node(info[0], info[1]));
         } else {
-            tree.resetChildren();
-            tree.getChildren().push(new Node(id, "Too Large"))
+
+            for (let i in root.getChildren()) {
+                if (root.getChildren()[i].getId() == parentId) {
+                    this.addNode(root.getChildren()[i], parent, node);
+                } else {
+                    this.addNode(root.getChildren()[i], parent, node);
+                }
+            }
         }
-        
-        return tree;
+        return root;
     };
 
 
@@ -75,16 +62,15 @@ class GenerateTree {
         let tree = this.findRootNode(list);
         //initial addition to JSON for d3 rendering
         
-        
-
         for (let n in nodeList) {
-            tree = this.addNode(tree, 0, nodeList[n]);
+            let info = nodeList[n].split(":");
+            let parent = info[0].substring(0, info[0].length - 1) + ":" + info[1]; 
+            tree = this.addNode(tree, parent , nodeList[n]);
         }
 
         this.json.id = tree.getId();
         this.json.name = tree.getName();
         this.json.children = tree.getChildren();
-
     
         return this.json;
     };
@@ -96,7 +82,7 @@ class RenderTree {
         let container = document.getElementById('thread-container');
         this.CONTAINER_WIDTH = container.clientWidth;
         this.CONTAINER_HEIGHT = container.clientHeight;
-        this.TREE_SCALE_X = 0.8;
+        this.TREE_SCALE_X = 0.9;
         this.TREE_SCALE_Y = 0.2;
         this.canvas = d3.select("thread-container").append('svg')
             .attr("width", this.CONTAINER_WIDTH)
@@ -129,23 +115,31 @@ class RenderTree {
 
         const root = d3.hierarchy(treeJSON);
         const links = treeLayout(root).links();
-        const linkPathGenerator = d3.linkHorizontal()
-            .x(d => d.y * this.TREE_SCALE_Y)
-            .y(d => d.x * this.TREE_SCALE_X);
+        const linkPathGenerator = d3.linkVertical()
+            .x(d => d.x * this.TREE_SCALE_X)
+            .y(d => d.y * this.TREE_SCALE_Y);
         
         //control rendering of graph elements
         g.selectAll('path').data(links).enter().append('path')
             .attr('d', linkPathGenerator);
         
         
-        
 
-        g.selectAll('text').data(root.descendants())
+        
+        
+        let tree = g.selectAll('text').data(root.descendants())
             .enter().append('text')
-                .attr('x', d => d.y * this.TREE_SCALE_Y)
-                .attr('y', d => d.x * this.TREE_SCALE_X)
-                .text(d => d.data.name)
-               // .on("mouseover", (e) => {console.log(e)});
+                .attr('x', d => d.x * this.TREE_SCALE_X)
+                .attr('y', d => d.y * this.TREE_SCALE_Y)
+                .attr("class", "event-text")
+                .attr("text-anchor", "middle")
+                .text(d =>  d.data.id + "\n" + d.data.name)
+
+                .on("click", (e) => {console.log("ASD")})
+
+
+        
+        
 
 
     }
@@ -156,9 +150,15 @@ class RenderTree {
 let tree = new GenerateTree();
 let children = []
 
-for (let x = 0; x < 13; x++) {
-    children.push("1i:asd" + x.toString())
-}
+
+children.push("1i:node");
+children.push("1ii:node");
+children.push("1iia:node");
+
+children.push("1iii:node");
+
+
+
 let result = tree.generateTree(children);
 let render = new RenderTree(result);
 
