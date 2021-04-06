3117ad209f2b5478230f1d06e17e8169d6e5571d
Dipraj Jimee <djimee1@sheffield.ac.uk>
2021-04-06 21:21:53 +0100

    add form partial + edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,25 @@
 Rails.application.routes.draw do
   mount EpiCas::Engine, at: "/"
   devise_for :users
-  
-  get 'test/index'
-  get 'edit/index'
-  match "/403", to: "errors#error_403", via: :all
+
+  resources :slipboxes
 
   get :"signin", to: "signin#index"
   get :"signup", to: "signup#index"
 
   get :"slipboxes", to: "slipboxes#index"
   get :"threads", to: "threads#index"
-  get :"mynotes", to: "mynotes#index"
+  get :"notes", to: "notes#index"
   
-  get :"test", to: "test#index"
   get :ie_warning, to: 'errors#ie_warning'
-
   get :javascript_warning, to: 'errors#javascript_warning'
-  
-  resources :slipboxes
 
-  # root to: "pages#home"
+  match "/403", to: "errors#error_403", via: :all
+  match "/404", to: "errors#error_404", via: :all
+  match "/422", to: "errors#error_422", via: :all
+  match "/500", to: "errors#error_500", via: :all
+
+  # root page - slipboxes index
   root to: "slipboxes#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
