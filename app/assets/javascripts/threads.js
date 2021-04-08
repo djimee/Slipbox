961e3f575dc8ebccb002a75ed7a726ed3214f8ab
James Horn <jhorn2@sheffield.ac.uk>
2021-04-08 22:10:51 +0100

    Implemneted recursion for non-existing nodes

--- app/assets/javascripts/threads.js
+++ app/assets/javascripts/threads.js
@@ -1,25 +1,24 @@
 "use strict"
-
+const baseRoot = "1:root";
 let currentRoot = "";
-let children = ["1:root"]
-
+let children = ["1:root"];
+let ids = [];
+let unassigned = [];
 
 children.push("1i:node");
 children.push("1ii:node");
 children.push("1iia:node");
 children.push("1iip:node");
 
-children.push("1iii:node");
+//children.push("1iii:node");
 children.push("1iiia:node");
-children.push("1iiib:node");
-children.push("1iiic:node");
-children.push("1iiid:node");
-children.push("1iiie:node");
-children.push("1iiif:node");
+
 
 
 //if delete a node then remove all information,
 //draw id as a 'filler' id on the thread tree
+
+const NODE_REC_LIMIT = 5;
 
 
 class Node {
@@ -53,22 +52,43 @@ class RecNode {
 
 }
 
+class FalseNode {
+
+    constructor(id) {
+        this.id = id;
+        this.children = [];
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getChildren() {
+        return this.children;
+    }
+    resetChildren() {
+        this.children = [];
+    }
+}
+
 class GenerateTree {
 
    constructor() {
        this.json = {};
        this.NODE_REC_LIMIT = 4 ;
+       this.loadIds();
    }
-
-    findRootNode(nodeList, root) {
+   loadIds() {
+     children.forEach((e) => { ids.push(e.split(":")[0]); });
+   }
+   findRootNode(nodeList, root) {
         let n =  null;
         nodeList.forEach((e) => {
            
         if (e.split(":")[0] == root.split(":")[0]){
             let items = e.split(":");
             n = new Node(items[0], items[1]);
-        }}
-);      
+        }});      
         currentRoot = n;
         return n;
     }; 
@@ -78,22 +98,31 @@ class GenerateTree {
         return false;
     }
 
+    findimmediateParent(node) {
+        let a = undefined;
+        ids.forEach((e) => {
+            if (e == node.substring(0, node.length - 1)) { a = e; }
+        });
+        return a;
+    }
+
+
     addNode(root, parent, node) { 
         let parentId = parent.split(":")[0];
+        
         if (root.getId() == parentId) {
             if (this.containsRecNode(root.getChildren())) {
                 root.getChildren()[0].increment();
             } else {
                 let info = node.split(":");
+                console.log(info[0]);
                 root.getChildren().push(new Node(info[0], info[1]));
             }
-            if (root.getChildren().length > this.NODE_REC_LIMIT && parent.split(":")[0] != currentRoot.getId()) {
+            if (root.getChildren().length > NODE_REC_LIMIT && parent.split(":")[0] != currentRoot.getId()) {
                 root.resetChildren();  
                 root.getChildren().push(new RecNode(root.getId()));
             }
-            
         } else {
-
             for (let i in root.getChildren()) {
                 if (root.getChildren()[i].getId() == parentId) {
                     this.addNode(root.getChildren()[i], parent, node);
@@ -105,19 +134,55 @@ class GenerateTree {
         return root;
     };
 
-    visitRecNode() {
-
+    addFalseNode(root, parentId, nodeId) {        
+        if (root.getId() == parentId) {
+            root.getChildren().push(new FalseNode(nodeId));    
+        } else {
+            for (let i in root.getChildren()) {
+                if (root.getChildren()[i].getId() == parentId) {
+                    this.addFalseNode(root.getChildren()[i], parentId, nodeId);
+                } else {
+                    this.addFalseNode(root.getChildren()[i], parentId, nodeId);
+                }
+            }
+        }
+        return root;
     }
-
 
     generateTree(nodeList, root) {
         let tree = this.findRootNode(nodeList, root);
-        console.log(tree);
         //initial addition to JSON for d3 rendering
         for (let n in nodeList) {
             let info = nodeList[n].split(":");
+
+            if (this.findimmediateParent(info[0]) == undefined && info[0].length > 1) {
+                unassigned.push(nodeList[n]);
+            }  else {
+                let parent = info[0].substring(0, info[0].length - 1) + ":" + info[1]; 
+                tree = this.addNode(tree, parent , nodeList[n]);
+            }
+        }
+
+        //Add unassigned nodes
+        for (let a in unassigned) {
+            let res = undefined
+            let nodesToAdd = [];
+            let info = unassigned[a].split(":");
+            let check = 1;
+            while (res == undefined) {
+                if (check < info[0].length) {
+                    res = this.findimmediateParent(info[0].substring(0, info[0].length - check));
+                    nodesToAdd.push(info[0].substring(0, info[0].length - check));
+                }
+                check++;
+            }
+            for (let n in nodesToAdd) {
+                let parent = nodesToAdd[n].substring(0, nodesToAdd[n].length - 1); 
+                tree = this.addFalseNode(tree, parent, nodesToAdd[n]);
+            }
             let parent = info[0].substring(0, info[0].length - 1) + ":" + info[1]; 
-            tree = this.addNode(tree, parent , nodeList[n]);
+
+            tree = this.addNode(tree, parent, unassigned[a]);
         }
 
         this.json.id = tree.getId();
@@ -136,6 +201,7 @@ class RenderTree {
         this.CONTAINER_HEIGHT = container.clientHeight;
         this.TREE_SCALE_X = 0.9;
         this.TREE_SCALE_Y = 0.2;
+        
         this.canvas = d3.select("thread-container").append('svg')
             .attr("width", this.CONTAINER_WIDTH)
             .attr("height", this.CONTAINER_HEIGHT)
@@ -147,21 +213,34 @@ class RenderTree {
 
     decideText(d) {
         if (d.data.id == undefined) {
-            return "+" +  d.data.nodeLength.toString() + " nodes"
+            return "+" +  ( NODE_REC_LIMIT + d.data.nodeLength).toString() + " nodes. Click to expand."
         }
-        return d.data.id + "\n" + d.data.name;
+        return d.data.id + " - " + d.data.name;
         
     }
 
+    regenerateTree(root) {
+        let newRoot = root;
+        d3.select("svg").selectAll("path").remove();
+        d3.select("svg").selectAll("text").remove();
+        let result = new GenerateTree().generateTree(children, newRoot.id + ":" + newRoot.name);
+        new RenderTree(result);
+    }
+
     clickHandler(e, i) {
-        if (i.data.id == undefined) {           //instanceof RecNode
-            d3.select("svg").selectAll("path").remove();
-            d3.select("svg").selectAll("text").remove();
-            let newRoot = i.parent.data;
-            console.log(newRoot);
-            console.log(children);
-            let result = new GenerateTree().generateTree(children, newRoot.id + ":" + newRoot.name);
-            new RenderTree(result);
+        if (i.data.id == undefined) {                       //instanceof RecNode
+                this.regenerateTree(i.parent.data)
+                d3.select("svg")
+                .append("text")
+                .text("<< Go back")
+                .attr("width", 100)
+                .attr("height", 100)
+                .attr("x", 5)
+                .attr("y", 15)
+                .attr("class", "event-text")
+                .on("click", (e) => {
+                    this.regenerateTree(new Node(baseRoot.split(":")[0], baseRoot.split(":")[1]))
+                });
         } else {
             //TODO - redirect to note page
         }
