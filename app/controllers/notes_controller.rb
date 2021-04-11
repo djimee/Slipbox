a8a26e0541314d4c758bf9c2f7fa33e2c54437a5
aca19aio <aiokan1@sheffield.ac.uk>
2021-04-11 17:03:10 +0300

    trying to work out create a note error, not done

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,28 +1,34 @@
 class NotesController < ApplicationController
     
     def index
-        @notes = Note.all.sort_by { |n| [n.unique_identifier] }
-        # @note = Note.new
+        # @notes = Note.all.sort_by { |n| [n.unique_identifier] }
+        @notes = Note.all
+        @note = Note.new
     end
 
-  
-    # def new
-    #     @note = Note.new(note_params)
+    def create
+        @note = Note.new(note_params)
     
-    #     if @note.save
-    #         redirect_to notes_path, notice: 'Note was created.'
-    #     else
-    #         @recent_note = Note.order("unique_identifier").last 
-    #         @notes = Note.all
-    #         render :index
-    #     end
-    # end
-    def new
-        render layout: false
+        if @note.save
+            redirect_to notes_path, notice: 'Note was created.'
+        else
+            @recent_note = Note.order("unique_identifier").last 
+            @notes = Note.all
+            render :index
+        end
     end
 
-    def note_params
-        params.permit(:title, :description, :unique_identifier)
-    end
+    # def new
+    #     render layout: false
+    # end
+    private 
+    # Use callbacks to share common setup or constraints between actions.
+        def set_note
+            @note = Note.find(params[:unique_identifier])
+        end
+
+        def note_params
+            params.require(:note).permit(:title, :content, :unique_identifier)
+        end
 
 end
