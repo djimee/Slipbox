260376a82e7dfc080c65f81a68c93b3ed26e855a
aca19aio <aiokan1@sheffield.ac.uk>
2021-04-12 15:55:59 +0300

    create a tree modification with controller

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -1,32 +1,47 @@
 class TreesController < ApplicationController
+    before_action :set_tree, only: [:show, :edit, :update, :destroy]
 
+    # GET /trees
     def index
         @trees = Tree.all
+    end
+
+    # GET /trees/new
+    def new
         @tree = Tree.new
     end
 
+    # GET /trees/new
     # redirects to the general tree page currently
     def show
         redirect_to trees_path
     end
 
+    # GET /trees/1/edit
+    def edit
+    end
+
+    # POST /trees
     def create 
         @tree = Tree.new(tree_params)
 
         if @tree.save
-            redirect_to trees_path, notice: 'Tree was created.'
+            redirect_to @tree, notice: 'Tree was successfully created.'
         else
-            @trees = Tree.all
-            render :index
+            render :new
         end
 
     end
 
     private
 
-        def tree_params
-            params.require(:tree).permit(:id, :title, :slipbox_id)
-        end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tree
+        @tree = Tree.find(params[:id])
+    end
 
+    def tree_params
+        params.require(:tree).permit(:title, :slipbox_id)
+    end
 
 end
