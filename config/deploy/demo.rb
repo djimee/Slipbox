29a68a246b4f95ce784b3b29aa6c62bb2d8134de
Dipraj Jimee <djimee1@sheffield.ac.uk>
2021-04-20 15:43:25 +0100

    start deployment

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -1,6 +1,6 @@
 ## Application deployment configuration
-set :server,      'DEMO_SERVER'
-set :user,        'DEMO_USER'
+set :server,      'epi-stu-hut-demo3.shef.ac.uk'
+set :user,        'demo.team19'
 set :deploy_to,   -> { "/srv/services/#{fetch(:user)}" }
 set :log_level,   :debug
 
@@ -15,3 +15,21 @@ namespace :deploy do
     end end end
   end
 end
+
+# ## Application deployment configuration
+# set :server,      'DEMO_SERVER'
+# set :user,        'DEMO_USER'
+# set :deploy_to,   -> { "/srv/services/#{fetch(:user)}" }
+# set :log_level,   :debug
+
+# ## Server configuration
+# server fetch(:server), user: fetch(:user), roles: %w{web app db}
+
+# ## Additional tasks
+# namespace :deploy do
+#   task :seed do
+#     on primary :db do within current_path do with rails_env: fetch(:stage) do
+#       execute :rake, 'db:seed'
+#     end end end
+#   end
+# end
