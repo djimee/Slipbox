6d11b01780877d02e33585c7e38ed72d0f0aee01
James Horn <jhorn2@sheffield.ac.uk>
2021-04-21 15:25:12 +0100

    Fixed some styling

--- db/schema.rb
+++ db/schema.rb
@@ -112,7 +112,6 @@ ActiveRecord::Schema.define(version: 2021_04_16_100805) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
-    t.integer "sort"
   end
 
   create_table "trees", force: :cascade do |t|
@@ -138,7 +137,6 @@ ActiveRecord::Schema.define(version: 2021_04_16_100805) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
-    t.string "remember_token"
     t.index ["email"], name: "index_users_on_email"
     t.index ["username"], name: "index_users_on_username"
   end
