efdaacbde946bde5a426641ce44ede117c81608f
Dipraj Jimee <djimee1@sheffield.ac.uk>
2021-04-22 16:54:11 +0100

    change notes + routes

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -1,55 +1,53 @@
 class TreesController < ApplicationController
-    before_action :set_tree, only: [:show, :edit, :update, :destroy]
+  before_action :set_tree, only: [:show, :edit, :update, :destroy]
 
-    # GET /trees
-    def index
-        @trees = Tree.order("updated_at")
-        @current_slipbox = Slipbox.new
+  # GET /trees
+  def index
+    @trees = Tree.order("updated_at")
+    @current_slipbox = Slipbox.new
+  end
+
+  # GET /trees/new
+  def new
+    @tree = Tree.new
+  end
+
+  # GET /trees/new
+  def show
+  end
+
+  # GET /trees/1/edit
+  def edit
+  end
+
+  def update
+    if @tree.update(tree_params)
+        @trees = Tree.all
+        redirect_to tree_path, notice: "Tree was updated."
+    else
+        redirect_to tree_path, notice: "There was a problem updating tree."
     end
+  end
 
-    # GET /trees/new
-    def new
-        @tree = Tree.new
+  # POST /trees
+  def create 
+    @tree = Tree.new(tree_params)
+
+    if @tree.save
+        redirect_to @tree, notice: 'Tree was successfully created.'
+    else
+        render :new
     end
+  end
 
-    # GET /trees/new
-    def show
-    end
-
-    # GET /trees/1/edit
-    def edit
-    end
-
-    def update
-        if @tree.update(tree_params)
-            @trees = Tree.all
-            redirect_to tree_path, notice: "Tree was updated."
-        else
-            redirect_to tree_path, notice: "There was a problem updating tree."
-        end
-    end
-
-    # POST /trees
-    def create 
-        @tree = Tree.new(tree_params)
-
-        if @tree.save
-            redirect_to @tree, notice: 'Tree was successfully created.'
-        else
-            render :new
-        end
-
-    end
-
-    private
-
+  private
     # Use callbacks to share common setup or constraints between actions.
     def set_tree
-        @tree = Tree.find(params[:id])
+      @tree = Tree.find(params[:id])
     end
 
     def tree_params
-        params.require(:tree).permit(:title, :slipbox_id)
+      params.require(:tree).permit(:title, :slipbox_id)
     end
 
 end
