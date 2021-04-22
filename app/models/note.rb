b29124067ba35437a351e67165bec975169edc33
Dipraj Jimee <djimee1@sheffield.ac.uk>
2021-04-22 12:02:07 +0100

    fix validation error

--- app/models/note.rb
+++ app/models/note.rb
@@ -17,7 +17,7 @@ class Note < ApplicationRecord
     has_rich_text :content
 
     # ensures there is content, unique title and a unique ID in the format of letter/number alternating
-    validates :unique_identifier, presence: true, uniqueness: { case_sensitive: false }
+    validates :unique_identifier, presence: true, uniqueness: { case_sensitive: false },
     format: { with: /\A^\b\d?(?:[a-z]\d)*[a-z]?$\z/, message: "unique ID must be in format: 1a2 for example - with letters in lower case"}
     validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 100 }
     validates :content, presence: true
