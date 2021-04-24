19f494c50fc4aaa4a63e5202b177102d4508cebf
Fanel Buliga <febuliga1@sheffield.ac.uk>
2021-04-24 21:49:49 +0300

    now able to zoom individual tree nodes by hovering over them

--- app/javascript/treeStructure/example/tree/exampleTree.js
+++ app/javascript/treeStructure/example/tree/exampleTree.js
@@ -9,6 +9,9 @@ var config = {
         }
     }
 
+chart_config = [] //important! - needed for the drawing engine
+
+
 //the root has no parent
 function makeRootNode(id, t, linkToNote = null) {
   node = {
@@ -16,7 +19,7 @@ function makeRootNode(id, t, linkToNote = null) {
     text: {
         //unique ID
         uniqueId: {
-          val: id.substring(1, id.length), //var names can't start with numbers, so we stock an 'n' before each ID
+          val: id.substring(1, id.length), //var names can't start with numbers, so we stick an 'n' before each ID
           href: linkToNote
         },
 
@@ -39,7 +42,7 @@ function makeChildNode(id, t, linkToNote = null) {
     text: {
         //uniqueID
         uniqueId: {
-          val: id.substring(1, id.length), //var names can't start with numbers, so we stock an 'n' before each ID
+          val: id.substring(1, id.length), //var names can't start with numbers, so we stick an 'n' before each ID
           href: linkToNote
         },
 
@@ -54,23 +57,18 @@ function makeChildNode(id, t, linkToNote = null) {
   return node;
 }
 
-nodeListString = ['1', '1a', '1a1', '1a2', '1a3','1a1a', '1a1b', '1a2a', '1a2b', '1a2c', '1a2c1', '1b', '1b1', '1b1a', '1b1b', '1b1a1']
-chart_config = [] //important! - needed for the drawing engine
+nodeListString = ['1', '1a', '1b', '1a1', '1a2', '1a3','1a1a', '1a1b', '1a2d', '1a2e', '1a2a', '1a2b', '1a2c', '1a2c1', '1b1', '1b1a', '1b1b', '1b1a1', '1b1a1b', '1b1a1b1', '1b1a1b1a', '1b1a1b1a1', '1b1a1b1a1a'];
 
 for(const node of nodeListString) {
   var nodeString =  'n' + node;
 
   if (node.length == 1) {
-    eval('var ' + nodeString + ' = ' + 'makeRootNode(nodeString, \'note title\')');
+    eval('var ' + nodeString + ' = ' + 'makeRootNode(nodeString, \'this is an example of a ROOT node right here in front of your eyes\',)');
   }
   else {
-    eval('var ' + nodeString + ' = ' + 'makeChildNode(nodeString, \'note title\')');
+    eval('var ' + nodeString + ' = ' + 'makeChildNode(nodeString, \'This is an example of a CHILD node right here in front of your eyes\')');
   }
-  // console.log(eval(nodeString));
   chart_config.push(eval(nodeString));
-  console.log(chart_config);
-  // i++;
-  // console.log(eval(nodeString) + "  " + i);
 }
 
 chart_config.push(config);
