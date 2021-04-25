0e3fba11f0836840dd0bedb11910304c12b22a3b
aca19aio <aiokan1@sheffield.ac.uk>
2021-04-25 13:03:19 +0300

    fixed back button routings on trees page to go to the slipbox

--- db/migrate/20210424204846_remove_column_title_from_reference.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnTitleFromReference < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :references, :title, :string
-  end
-end
--- db/migrate/20210424204934_remove_note_id_from_reference.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNoteIdFromReference < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :references, :note_id, :bigint
-  end
-end
