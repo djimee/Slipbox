0fc0c0d9ad5a4aba17a77e50435fdce73648e847
Dipraj Jimee <djimee1@sheffield.ac.uk>
2021-04-30 14:57:43 +0100

    finishh refs apart from refreshing partial after updating

--- spec/factories/references.rb
+++ spec/factories/references.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: references
+#
+#  id               :bigint           not null, primary key
+#  author           :string           not null
+#  content          :string
+#  publication_year :integer
+#  sort             :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 FactoryBot.define do
   factory :reference do
     
--- spec/models/reference_spec.rb
+++ spec/models/reference_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: references
+#
+#  id               :bigint           not null, primary key
+#  author           :string           not null
+#  content          :string
+#  publication_year :integer
+#  sort             :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Reference, type: :model do
