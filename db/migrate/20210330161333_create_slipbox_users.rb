8856c89889fbae40b3e4cfb5be411a6db81039fd
aca19aio <aiokan1@sheffield.ac.uk>
2021-05-20 17:01:36 +0100

    deleted slipboxes_users table

--- db/migrate/20210330161333_create_slipbox_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateSlipboxUsers < ActiveRecord::Migration[6.0]
-  def change
-    create_table :slipbox_users, id: false do |t|
-      t.boolean :admin
-
-      t.timestamps
-    end
-  end
-end
